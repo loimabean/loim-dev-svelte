50d15c99051b5b099596834a43a31f3f09631a0c
Liam Rust <liamrust@proton.me>
2026-03-12 21:10:17 -0400

    initial commit
    just a basic placeholder site to get me started!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+</script>
+
+<svelte:head>
+	<title>hi, i'm liam!</title>
+</svelte:head>
+
+<div class="flex h-screen w-screen flex-col items-center justify-center">
+	<h1 class="text-4xl font-bold">under construction</h1>
+	<h2 class="text-2xl">hi, i'm liam!</h2>
+	<img src={favicon} alt="an orange circle with a smiling hexagon face inside" class="my-4 h-64" />
+	<p>this will be my corner of the internet Soon™</p>
+	<p>but until then, it's under construction :)</p>
+	<ul>
+		<li>bluesky: <a href="https://bsky.app/profile/loim.dev">@loim.dev</a></li>
+	</ul>
+</div>
