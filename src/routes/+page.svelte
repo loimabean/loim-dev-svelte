24f2baf23a88a02b3da46225cbfd26fbc3bdcc4d
Liam Rust <liamrustm300@gmail.com>
2026-03-13 01:02:04 -0400

    add inter font, new email, and better styling
    there's a slight unstyled-font flash as the page is loaded, at least on
    the local dev version. wanna look into improving that later.
    
    chose some better colors for the site as well. and people can email me
    now!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,11 @@
 <div class="flex h-screen w-screen flex-col items-center justify-center">
 	<h1 class="text-4xl font-bold">under construction</h1>
 	<h2 class="text-2xl">hi, i'm liam!</h2>
-	<img src={favicon} alt="an orange circle with a smiling hexagon face inside" class="my-4 h-64" />
+	<img src={favicon} alt="an orange circle with a smiling hexagon-shaped face" class="my-4 h-56" />
 	<p>this will be my corner of the internet Soon™</p>
 	<p>but until then, it's under construction :)</p>
-	<ul>
+	<ul class="mt-8">
 		<li>bluesky: <a href="https://bsky.app/profile/loim.dev">@loim.dev</a></li>
+		<li>email: <a href="mailto:hi@loim.dev">hi@loim.dev</a></li>
 	</ul>
 </div>
