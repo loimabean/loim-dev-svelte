5d537a7af605430fc2334bca65e505a3ec431715
Liam Rust <liamrust@proton.me>
2026-03-15 16:59:37 -0400

    beginnings of a proper about me page!
    added at *least* one other proper page for the navbar (not done with it
    at all yet, but i want to get this navbar merged)
    
    also i made the error page a little nicer

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,8 @@
 	import { page } from '$app/state';
 </script>
 
-<div>
-	<h1 class="mb-4 text-4xl">you got an error! {page.error?.message}</h1>
-	<p>if you didn't expect that, let me know...</p>
+<div class="flex flex-col items-center">
+	<h1 class="mb-8 text-4xl font-bold">you got an error!</h1>
+	<h2 class="mb-4 text-2xl">{page.error?.message}</h2>
+	<p>if you didn't expect that, let me know</p>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
-	let nums: number[] = [];
-	for (let i = 0; i < 1000; i++) {
-		nums.push(i);
-	}
+	import { differenceInYears } from 'date-fns';
+	import me from '$lib/assets/me.webp';
+
+	const birthday = new Date(2002, 10, 3);
+	const today = new Date();
+	const age = differenceInYears(today, birthday);
 </script>
 
-<div>
-	{#each nums as num (num)}
-		<p>{num}</p>
-	{/each}
-	<h1>about me!</h1>
-	<p>hi!</p>
+<svelte:head>
+	<title>about me</title>
+	<meta property="og:title" content="about me" />
+	<meta
+		property="og:description"
+		content="information about me, liam! (the creator of this site)"
+	/>
+</svelte:head>
+
+<div class="max-w-2xl">
+	<h1 class="mb-4 text-4xl font-bold">about me</h1>
+	<p>
+		i'm <strong class="text-amber-700 dark:text-amber-400">liam rust</strong>!
+		<span class="text-taupe-700 dark:text-taupe-300">(pronouns: he/she/they)</span>
+	</p>
+	<p>i'm a {age} year old graduate computer science student at Stevens Institute of Technology.</p>
+	<figure class="my-4 flex flex-col items-center">
+		<img
+			src={me}
+			alt="A white androgynous person in a motorized wheelchair. They have medium-long, dirty blonde hair, round gold glasses, and star-shaped earrings. They are smiling."
+			class="mb-2 h-192 rounded-xl"
+		/>
+		<figcaption class="italic">that's me!</figcaption>
+	</figure>
 </div>
