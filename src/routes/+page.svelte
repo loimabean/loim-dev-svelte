d8554378c263ad54345e0301f7a52a16e41b4034
Liam Rust <liamrust@proton.me>
2026-04-17 02:37:12 -0400

    update blog styling + steal it for homepage
    probably should make this some kind of element lol. but it looks nice
    and that's all that matters!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import bluesky from '$lib/assets/bluesky.svg';
+	import github_black from '$lib/assets/github-black.svg';
+	import github_white from '$lib/assets/github-white.svg';
+	import mail from '$lib/assets/mail.svg';
+	import linkedin from '$lib/assets/linkedin.svg';
 </script>
 
 <svelte:head>
@@ -8,34 +13,76 @@
 	<meta property="og:description" content="welcome to my corner of the internet :)" />
 </svelte:head>
 
-<main class="flex flex-col items-center justify-center">
+<main class="flex w-full max-w-xl flex-col items-center justify-center">
 	<h1 class="text-4xl font-bold">hi, i'm liam!</h1>
 	<img src={favicon} alt="an orange circle with a smiling hexagon-shaped face" class="my-4 h-56" />
-	<p>this will be my corner of the internet Soon™</p>
-	<p>but until then, it's under construction :)</p>
-	<address class="mt-8 not-italic">
-		<!-- TODO make a link element for this kind of thing -->
-		<p>
-			bluesky: <a
-				class="text-amber-800 underline transition-colors visited:text-red-700 active:text-amber-900 dark:text-amber-300 dark:visited:text-red-400 dark:active:text-amber-200"
-				target="_blank"
-				href="https://bsky.app/profile/loim.dev">@loim.dev</a
+	<p>welcome to my corner of the internet!</p>
+	<p>i'm still working on this site, but feel free to look around :)</p>
+	<ul class="mt-4 w-full space-y-4">
+		<li>
+			<a
+				href="https://bsky.app/profile/loim.dev"
+				class="group flex w-full items-center gap-4 rounded-2xl border-3 border-taupe-200 p-4 transition-all hover:bg-taupe-50 hover:shadow active:bg-taupe-50 active:shadow dark:border-taupe-800 hover:dark:bg-taupe-800 active:dark:bg-taupe-800"
 			>
-		</p>
-		<p>
-			email: <a
-				class="text-amber-800 underline visited:text-red-700 active:text-amber-950 dark:text-amber-300 dark:visited:text-red-400 dark:active:text-amber-100"
-				href="mailto:liam@loim.dev">liam@loim.dev</a
-			>
-		</p>
-		<p>
-			website source: <a
-				class="text-amber-800 underline visited:text-red-700 active:text-amber-950 dark:text-amber-300 dark:visited:text-red-400 dark:active:text-amber-100"
-				target="_blank"
-				href="https://github.com/loimabean/loim-dev-svelte"
-			>
-				https://github.com/loimabean/loim-dev-svelte
+				<img src={bluesky} alt="BlueSky logo" class="w-12" />
+				<div>
+					<h2
+						class="text-2xl font-semibold transition-all group-hover:text-amber-700 group-active:text-amber-700 active:font-bold dark:group-hover:text-amber-400 dark:group-active:text-amber-400"
+					>
+						@loim.dev
+					</h2>
+					<p class="mt-2 text-taupe-700 dark:text-taupe-400">bluesky</p>
+				</div>
 			</a>
-		</p>
-	</address>
+		</li>
+		<li>
+			<a
+				href="mailto:liam@loim.dev"
+				class="group flex w-full items-center gap-4 rounded-2xl border-3 border-taupe-200 p-4 transition-all hover:bg-taupe-50 hover:shadow active:bg-taupe-50 active:shadow dark:border-taupe-800 hover:dark:bg-taupe-800 active:dark:bg-taupe-800"
+			>
+				<img src={mail} alt="ProtonMail logo" class="w-12" />
+				<div>
+					<h2
+						class="text-2xl font-semibold transition-all group-hover:text-amber-700 group-active:text-amber-700 active:font-bold dark:group-hover:text-amber-400 dark:group-active:text-amber-400"
+					>
+						liam@loim.dev
+					</h2>
+					<p class="mt-2 text-taupe-700 dark:text-taupe-400">email</p>
+				</div>
+			</a>
+		</li>
+		<li>
+			<a
+				href="https://www.linkedin.com/in/liam-rust-652802235/"
+				class="group flex w-full items-center gap-4 rounded-2xl border-3 border-taupe-200 p-4 transition-all hover:bg-taupe-50 hover:shadow active:bg-taupe-50 active:shadow dark:border-taupe-800 hover:dark:bg-taupe-800 active:dark:bg-taupe-800"
+			>
+				<img src={linkedin} alt="LinkedIn logo" class="w-12" />
+				<div>
+					<h2
+						class="text-2xl font-semibold transition-all group-hover:text-amber-700 group-active:text-amber-700 active:font-bold dark:group-hover:text-amber-400 dark:group-active:text-amber-400"
+					>
+						Liam Rust
+					</h2>
+					<p class="mt-2 text-taupe-700 dark:text-taupe-400">linkedin</p>
+				</div>
+			</a>
+		</li>
+		<li>
+			<a
+				href="https://github.com/loimabean/loim-dev-svelte"
+				class="group flex w-full items-center gap-4 rounded-2xl border-3 border-taupe-200 p-4 transition-all hover:bg-taupe-50 hover:shadow active:bg-taupe-50 active:shadow dark:border-taupe-800 hover:dark:bg-taupe-800 active:dark:bg-taupe-800"
+			>
+				<img src={github_white} alt="LinkedIn logo" class="hidden w-12 dark:block" />
+				<img src={github_black} alt="LinkedIn logo" class="block w-12 dark:hidden" />
+				<div>
+					<h2
+						class="text-2xl font-semibold transition-all group-hover:text-amber-700 group-active:text-amber-700 active:font-bold dark:group-hover:text-amber-400 dark:group-active:text-amber-400"
+					>
+						loimabean/loim-dev-svelte
+					</h2>
+					<p class="mt-2 text-taupe-700 dark:text-taupe-400">website source (on github)</p>
+				</div>
+			</a>
+		</li>
+	</ul>
 </main>
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -12,18 +12,19 @@
 </svelte:head>
 
 <main class="w-full max-w-xl">
-	<h1 class="mb-4 ml-4 text-4xl font-bold">blog</h1>
-	<ul class="w-full space-y-6">
+	<h1 class="mb-8 text-4xl font-bold">blog</h1>
+	<ul class="w-full space-y-4">
 		{#each data.posts as post (post.slug)}
-			<li
-				class="group rounded-2xl transition hover:bg-taupe-50 hover:shadow hover:dark:bg-taupe-800"
-			>
-				<a href={resolve(`/blog/${post.slug}`)} class="inline-block w-full p-4">
+			<li>
+				<a
+					href={resolve(`/blog/${post.slug}`)}
+					class="group inline-block w-full rounded-2xl border-3 border-taupe-200 p-4 transition-all hover:bg-taupe-50 hover:shadow active:bg-taupe-50 active:shadow dark:border-taupe-800 hover:dark:bg-taupe-800 active:dark:bg-taupe-800"
+				>
 					<p class="mb-2 text-sm text-taupe-500">
 						{formatDate(post.date, 'MMMM do, yyyy')}
 					</p>
 					<h2
-						class="text-2xl font-semibold transition group-hover:text-amber-700 dark:group-hover:text-amber-400"
+						class="text-2xl font-semibold transition-all group-hover:text-amber-700 group-active:text-amber-700 active:font-bold dark:group-hover:text-amber-400 dark:group-active:text-amber-400"
 					>
 						{post.title}
 					</h2>
